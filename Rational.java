8a94649a074305bd2cc5f00742897e24c56cb404
CC00001 <zuyundasi@gmail.com>
2019-07-10 22:59:05 -0700

    Rational.java updated on 07/10/2019

--- Rational.java
+++ Rational.java
@@ -1,17 +1,22 @@
 class Rational{
 
- private int numerator, denominator;
- private static int wholeNumber;
-
+ private int numerator, denominator, wholeNumber;
+ 
  public Rational(int n, int d){
    int g = greatestCommonDenominator(n, d);//Simplify fraction by using common Denominator
-   if(d == 0) System.out.println("Denominator cannot be 0.");
-   else numerator = n / g; denominator = d / g;
+   if(d == 0) 
+   System.out.println("Denominator cannot be zero.");
+   numerator = n / g; denominator = d / g;
+ 
+   /*if(d < 0){
+    numerator = -numerator; 
+    denominator = -denominator;
+    }*/
  }
 
  public Rational(int w){
-  if(w == 0) System.out.println("Denominator couldnot be 0");
-  else {this.wholeNumber = w;} 
+  //if(w == 0) System.out.println("Denominator couldnot be 0");
+  wholeNumber = w; wholeNumber /= 1;
  }
 
  public Rational(){
@@ -34,27 +39,48 @@ class Rational{
   return new Rational((a.numerator * b.denominator + b.numerator * a.denominator), a.denominator * b.denominator);
  }
  
- public static Rational addition(Rational b){//Overloading method with only one parameter
-  return new Rational(wholeNumber + b.wholeNumber);
+ public Rational addition(Rational b){//Overloading method with only one parameter, using "this" to indicate the object 'a' itself
+  if(this.denominator == b.denominator)
+  return new Rational(this.numerator + b.numerator, this.denominator);
+  else
+  return new Rational(this.numerator * b.denominator + b.numerator * this.denominator, this.denominator * b.denominator);
  }
 
- public static int subtraction(int a, int b){
-  return a - b;
+ public static Rational subtraction(Rational a, Rational b){
+  if(a.denominator == b.denominator)
+  return new Rational(a.numerator - b.numerator, a.denominator);
+
+  return new Rational((a.numerator * b.denominator - b.numerator * a.denominator), a.denominator * b.denominator);
  }
 
- public static int multiplication(int a, int b){
-  return a * b;
+ public Rational subtraction(Rational b){
+  if(this.denominator == b.denominator)
+  return new Rational(this.numerator + b.numerator, this.denominator);
+  
+  return new Rational(this.numerator * b.denominator - b.numerator * this.denominator, this.denominator * b.denominator);
  }
 
- public static int division(int a, int b){
-  return a / b;
+ public static Rational multiplication(Rational a, Rational b){
+  return new Rational(a.numerator * b.numerator, a.denominator * b.denominator);
  }
 
- public static int normalize(int a, int b){
-  if(a < 0) a = a;
-  else a = -a;
-  if(b < 0) b = -b;
-  return normalize(a, b);
+ public Rational multiplication(Rational b){
+  return new Rational(this.numerator * b.numerator, this.denominator * b.denominator);
+ }
+
+ public static Rational division(Rational a, Rational b){
+  return new Rational(a.numerator / b.numerator, a.denominator / b. denominator);
+ }
+
+ public Rational division(Rational b){
+  return new Rational(this.numerator / b.numerator, this.denominator * b.denominator);
+ }
+
+ public static Rational normalize(Rational a){
+  if(a.denominator < 0){
+  a.numerator *= -1; a.denominator *= -1;
+  }
+  return a;
  }
 
  public static boolean isEquals(Rational a, Rational b){
@@ -64,24 +90,36 @@ class Rational{
  }
 
  public String toString(){
-  return (this.numerator + "/" + this.denominator + ", the wholenumber is: " + wholeNumber);
+  if(denominator == 1)
+  return (numerator + " ");
+  else{
+  //normalize(this.numerator, this.denominator);
+  return (this.numerator + "/" + this.denominator);
+  }
  }
 
  
  public static void main(String [] args){
-  Rational r1 = new Rational(3, 8);
-  Rational r2 = new Rational(9, 5);
-  Rational r3 = new Rational(8);
-  Rational r4 = new Rational(3);
- 
-  System.out.println(r1.numerator + " " + r1.denominator + " " + r2.numerator + " " + r2.denominator);
+  Rational r1 = new Rational(1, 6);
+  Rational r2 = new Rational(-4, -8);
+  Rational r3 = new Rational(10, 5);
+  Rational r4 = new Rational(4, 2);
+  
+  System.out.println("Rational number 1: " + r1.toString() + ", Rational number 2: " + r2.toString());
   System.out.println("Greast common Denominator is:" + greatestCommonDenominator(r1.denominator, r2.denominator));
   
-  System.out.println(r1);
+  System.out.println("WholeNumber:" + r3);
   System.out.println(r2);
-  System.out.println(addition(r1, r2));
-  System.out.println(r3.addition(r4));
-  //System.out.println(r1.addition(b));
+  System.out.println("addition(x, y) version:" + normalize(addition(r1, r2)));
+  System.out.println("x.addition(y) version:" + normalize(r1.addition(r2)));
+  System.out.println("Subtraction(x, y) version:" + normalize(subtraction(r1, r2)));
+  System.out.println("x.subtraction(y):" + normalize(r1.subtraction(r2)));
+  System.out.println("multiplicaton(x, y) version:" + normalize(multiplication(r3, r4)));
+  System.out.println("x.multiplication(y) version:" + normalize(r3.multiplication(r4)));
+  System.out.println("division(r3, r4) version:" + normalize(division(r3, r4)));
+  System.out.println("x.division(y) version:" + normalize(r3.division(r4)));
+
 
  }
 }
+
