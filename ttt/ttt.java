83e3f2d1745371779d9645a7dc2b2acfc8dc2259
CC00001 <zuyundasi@gmail.com>
2019-07-30 15:46:04 -0700

    Udpateded

--- ttt/ttt.java
+++ ttt/ttt.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 class ttt extends JFrame{
 
@@ -11,12 +11,47 @@ class ttt extends JFrame{
 
  public ttt(){ //Constructor
   JPanel content = new JPanel();
-  content.setLayout(new BorderLayout());
-  content.add(_tttBoard, BorderLayout.CENTER);
+  JButton one = new JButton("");
+  JButton two = new JButton("");
+  JButton three = new JButton("");
+  JButton four = new JButton("");
+  JButton five = new JButton("");
+  JButton six = new JButton("");
+  JButton seven = new JButton("");
+  JButton eight = new JButton("");
+  JButton nine = new JButton("");
+  content.setLayout(new GridLayout(3, 3));
+  content.add(one);
+  content.add(two);
+  content.add(three);
+  content.add(four);
+  content.add(five);
+  content.add(six);
+  content.add(seven);
+  content.add(eight);
+  content.add(nine);
+
+  //Add Listener
+  one.addActionListener(new oneListener());
+  //two.addActionListener(new twoListener());
+  //three.addActionListener(new threeListener());
+  //four.addActionListener(new fourListener());
+  //five.addActionListener(new fiveListener());
+  //six.addActionListener(new sixListener());
+  //seven.addActionListener(new sevenListener());
+  //eight.addActionListener(new eightListener());
+  //nine.addActionListener(new nineListener());
+ 
   setContentPane(content);
   setTitle("Tac-Tic-Toe");
   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
+ }
+ //Implement Listensers
+ class oneListener implements ActionListener{
+  public void actionPerformed(ActionEvent e){
+   e.setText("O"); 
+  }
  }
 
  public static void main(String [] args){
